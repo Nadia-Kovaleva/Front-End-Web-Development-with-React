900175084128be8d633d83e112973cb99df1eb58
Nadezhda Kovaleva <Nadia.kv13@gmail.com>
2020-11-03 17:22:50 +0300

    Assignment 1. Some moves in the storage.

---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+  renderDish(dish) {
+    if (dish !=null) {
+      return(
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name}/>
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    }
+    else {
+      return(
+        <div></div>
+      )
+    }
+  }
+
+  renderComments(comments) {
+    if (comments != null) {
+        const commentsList = this.props.selectedDish.comments.map((comment) => {
+          return (
+            <ul className='list-unstyled' key={comment.id}>
+              <li>
+                  <p>{comment.comment}</p>
+                  <p>--{comment.author},{comment.date}</p>
+              </li>
+            </ul>
+          );
+        });
+        return (
+          <div>
+              <h4>Comments</h4>
+              {commentsList}
+          </div>
+        );
+    } else {
+        return(
+            <div></div>
+        );
+      }
+    }
+
+  render() { 
+    return (
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          {this.renderDish(this.props.selectedDish)}
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          {this.renderComments(this.props.selectedDish)}
+        </div>
+      </div>
+    )
+  }
+};
+
+export default DishDetail;
