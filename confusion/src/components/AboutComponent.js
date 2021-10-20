481f8b8c6e0ab4b128c36acf5b21239b8a6ca986
Nadia Kovaleva <nadezhda.kovaleva@abs-auto.net>
2021-10-20 12:44:38 +0300

    Final task

--- confusion/src/components/AboutComponent.js
+++ confusion/src/components/AboutComponent.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderLeader({leader}) {
     return(
-      <Media>
-        <Media left top>
-            <Media object src={leader.image} alt={leader.name} />
+        <Media>
+          <Media left top>
+              <Media object src={baseUrl + leader.image} alt={leader.name} />
+          </Media>
+          <Media body className="ml-5">
+              <Media heading>{leader.name}</Media>
+              <p>{leader.designation}</p>
+              <p>{leader.description}</p>
+          </Media>
         </Media>
-        <Media body className="ml-5">
-            <Media heading>{leader.name}</Media>
-            <p>{leader.designation}</p>
-            <p>{leader.description}</p>
-        </Media>
-      </Media>
     );
   }
 
 function About(props) {
-
-    const leaders = props.leaders.map((leader) => {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    } 
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
             <div className="col-12 m-1" key={leader.id}>
                 <RenderLeader leader={leader} />
@@ -83,7 +103,11 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {leaders}
+                        <Stagger in>    
+                            <Fade in>
+                                {leaders}
+                            </Fade>
+                        </Stagger>
                     </Media>
                 </div>
             </div>
